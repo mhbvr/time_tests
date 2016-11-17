0d057d74b447da41db2c5f82b62a7b98f28797b1
Mikhail Kulemin <mikhail.kulemin@moex.com>
2016-11-17 21:58:51 +0300

    Stat implementation in C++

--- stat.cpp
+++ stat.cpp
@@ -2,32 +2,144 @@
 // Created by mih on 11/11/16.
 //
 
-#include <time.h>
+
+#include <random>
+#include <iostream>
 #include "stat.h"
 
 
-class Histrogram {
-    uint64_t tot_points;
-    uint32_t max_buckets;
-    uint32_t max_outliers;
-    uint8_t log_interval;
-    uint32_t tot_outliers;
-    uint32_t i_outlier;
-    uint64_t sum;
-    uint64_t sqr_sum;
-    uint64_t *buckets;
-    uint64_t *outliers;
-
+class Point {
 public:
-    void add_value(uint64_t value){
+    Point(struct timespec timestamp, uint64_t value):
+            timestamp(timestamp),
+            value(value) {}
+    Point() = default;
 
-    }
-
-    void add_value(uint64_t value, str  uct timespec timestamp){
-
-    }
+    struct timespec timestamp;
+    uint64_t value;
 };
 
+
+class LatHist {
+    uint64_t max_buckets;
+    uint64_t max_outliers;
+    uint8_t log_interval;
+    uint64_t tot_outliers = 0;
+    uint64_t sum = 0;
+    uint64_t sqr_sum = 0;
+    std::vector<uint64_t> buckets;
+    std::vector<Point> outliers;
+
+public:
+    LatHist(uint32_t max_buckets, uint8_t log_interval, uint32_t max_outliers):
+            max_buckets(max_buckets),
+            max_outliers(max_outliers),
+            log_interval(log_interval),
+            buckets(max_buckets, 0),
+            outliers(max_outliers) {}
+    uint64_t min_value = 0;
+    uint64_t max_value = 0;
+    uint64_t tot_points = 0;
+
+    void add_value(uint64_t value){
+        struct timespec timestamp;
+        int err;
+        err = clock_gettime(CLOCK_REALTIME, &timestamp);
+        if (err < 0) {
+            perror("clock_gettime()");
+            return;
+        }
+        add_value(value, timestamp);
+    }
+
+    void add_value(uint64_t value, struct timespec timestamp){
+        uint64_t max_hist_value;
+        uint64_t bucket;
+
+        ++tot_points;
+        sum += value;
+        sqr_sum += value*value;
+        if (value>max_value) {
+            max_value = value;
+        }
+
+        if (value < min_value) {
+            min_value = value;
+        }
+        max_hist_value = (1 << log_interval) * max_buckets;
+        if (value > max_hist_value) {
+            if (tot_outliers < max_outliers) {
+                outliers[tot_outliers] = (Point(timestamp, value));
+            }
+            ++tot_outliers;
+        } else {
+            bucket = value/(1 << log_interval);
+            ++buckets[bucket];
+        }
+    }
+
+    double avg() {
+        if (tot_points > 0){
+            return sum/tot_points;
+        } else {
+            return 0;
+        }
+    }
+
+    double sigma() {
+        if (tot_points > 1){
+            return std::sqrt(sqr_sum/(tot_points-1) - (sum/(tot_points-1))*(sum/(tot_points-1)));
+        } else {
+            return 0;
+        }
+    }
+
+    friend std::ostream &operator<<(std::ostream &os, LatHist &hist) {
+        uint64_t n = 0;
+        Point p;
+        os << "# Number of points: " << hist.tot_points << std::endl;
+        os << "# Max value: " << hist.max_value << std::endl;
+        os << "# Min value: " << hist.min_value << std::endl;
+        os << "# Average: " << hist.avg() << std::endl;
+        os << "# Sigma: " << hist.sigma() << std::endl;
+        os << "# Number of outliers (>" << hist.max_buckets*(1 << hist.log_interval) << "): " << hist.tot_outliers << std::endl;
+        os << "# Histogram:" << std::endl;
+        for (uint64_t val : hist.buckets) {
+            os << n * (1 << hist.log_interval) << " " << (n + 1) * (1 << hist.log_interval) << " " << val << std::endl;
+            ++n;
+        }
+        os << "# Outliers:" << std::endl;
+        for (uint64_t i = 0; i < hist.tot_outliers; i++) {
+            p = hist.outliers[i];
+            os << p.timestamp.tv_sec * 1000000000 +  p.timestamp.tv_nsec << " " << p.value << std::endl;
+        }
+        return os;
+    }
+
+};
+
+
 int main(int argv, char**argc) {
-    return 0;
+    std::random_device rd;
+    std::normal_distribution<> distrib(200,60);
+    int i;
+    struct timespec start, end, tmp;
+
+    LatHist hist(100, 2, 1000);
+    LatHist hist2(100, 2, 1000);
+//    for(i=0; i<100000; i++) {
+//        hist.add_value(std::round(std::abs(distrib(rd))));
+//    }
+//    std::cout << hist;
+
+
+    for(i=0; i<1000000; i++) {
+        tmp = start;
+        clock_gettime(CLOCK_REALTIME, &start);
+        if (i > 0)
+            hist.add_value(delta_ns(tmp, end), start);
+        clock_gettime(CLOCK_REALTIME, &end);
+    }
+    std::cout << hist;
+
 }
--- stat.h
+++ stat.h
@@ -6,7 +6,12 @@
 #define TIME_TESTS_STAT_H
 
 #include <stdint.h>
+#include <time.h>
+#include <cstdio>
+#include <cmath>
 #include <vector>
 
-
+static inline long delta_ns(struct timespec begin, struct timespec end) {
+    return (end.tv_sec - begin.tv_sec) * 1000000000 + end.tv_nsec - begin.tv_nsec;
+}
 #endif //TIME_TESTS_STAT_H
